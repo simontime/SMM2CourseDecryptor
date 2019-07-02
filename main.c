42b8a3d898843c96f0f791c7fcbdf441abcf7ddc
Blake Warner <blake@null3d.com>
2019-07-02 18:41:00 -0400

    added support for other file types, and refactored.

--- main.c
+++ main.c
@@ -10,6 +10,15 @@
 
 #define SWAP(x) ((x >> 24) & 0xff | (x >> 8) & 0xff00 | (x << 8) & 0xff0000 | (x << 24) & 0xff000000)
 
+struct mm_file_type
+{
+	char* name;
+	char* fileName;
+	size_t fileSize;
+	size_t offset;
+	uint32_t* key_table;
+};
+
 void rand_init(uint32_t *rand_state, uint32_t in1, uint32_t in2, uint32_t in3, uint32_t in4)
 {
 	int cond = in1 | in2 | in3 | in4;
@@ -45,16 +54,92 @@ void gen_key(uint32_t *key_table, uint32_t *out_key, uint32_t *rand_state)
 	}
 }
 
-int main(int argc, char **argv)
+int usage(int argc, char **argv)
 {
-	if (argc != 3)
+	printf("Usage: %s input output\n", argv[0]);
+	return -1;
+}
+
+char endsWith(const char* a, const char* b)
+{
+	if (!a || !b)
 	{
-		printf("Usage: %s input output\n", argv[0]);
 		return 0;
 	}
+
+	int lenA = strlen(a);
+	int lenB = strlen(b);
+
+	if (lenB > lenA)
+	{
+		return 0;
+	}
+
+	return strcmp(a + lenA - lenB, b) == 0;
+}
+
+struct mm_file_type file_types[] = {
+	{ "save", "save.dat", 0xC000, 0x0, save_key_table },
+	{ "quest", "quest.dat", 0xC000, 0x0, quest_key_table },
+	{ "later", "later.dat", 0xC000, 0x0, later_key_table },
+	{ "replay", ".dat", 0x68000, 0x0, replay_key_table },
+	{ "network", "network.dat", 0x48000, 0x0, network_key_table },
+	{ "thumb", ".btl", 0x1C000, 0x0, thumb_key_table },
+	{ "course", ".bcd", 0x5C000, 0x10, course_key_table }
+};
+
+struct mm_file_type* getFileType(const char* fileName, size_t size)
+{
+	for (int i = 0; i < sizeof(file_types) / sizeof(struct mm_file_type); i++)
+	{
+		if (file_types[i].fileSize == size && endsWith(fileName, file_types[i].fileName))
+		{
+			return &file_types[i];
+		}
+	}
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	char encrypt = 0;
+	char* inFileName = NULL;
+	char* outFileName = NULL;
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == '-')
+		{
+			switch (argv[i][1])
+			{
+			case 'e':
+				encrypt = 1;
+				break;
+			default:
+				return usage(argc, argv);
+			}
+		}
+		else if(!inFileName)
+		{
+			inFileName = argv[i];
+		}
+		else if (!outFileName)
+		{
+			outFileName = argv[i];
+		}
+		else
+		{
+			return usage(argc, argv);
+		}
+	}
+
+	if (!inFileName || !outFileName)
+	{
+		return usage(argc, argv);
+	}
 	
-	FILE *in = fopen(argv[1], "rb");
-	FILE *out = fopen(argv[2], "wb");
+	FILE *in = fopen(inFileName, "rb");
+	FILE *out = fopen(outFileName, "wb");
 	
 	fseek(in, 0, SEEK_END);
 	size_t sz = ftell(in);
@@ -64,92 +149,44 @@ int main(int argc, char **argv)
 	
 	uint32_t rand_state[STATE_SIZE];
 	uint32_t key_state[STATE_SIZE];
-	
-	switch(sz) // CMAC calculation isn't handled yet, will implement eventually
+
+
+	struct mm_file_type* file_type = getFileType(inFileName, sz);
+
+	if (!file_type)
 	{
-		case 0x1C000: // Thumbnail image data
-		{
-			printf("Decrypting thumbnail %s to %s...\n", argv[1], argv[2]);
-			
-			char *buf = malloc(0x1C000);
-			
-			fread(buf, 1, 0x1C000, in);
-			fclose(in);
-			
-			char *end = buf + sz - 0x30;
-			
-			rand_init(rand_state, *(uint32_t *)&end[0x10], *(uint32_t *)&end[0x14], *(uint32_t *)&end[0x18], *(uint32_t *)&end[0x1C]);
-			gen_key(thumb_key_table, key_state, rand_state);
-			
-			AES_init_ctx_iv(&ctx, (uint8_t *)key_state, end);		
-			AES_CBC_decrypt_buffer(&ctx, buf, sz - 0x30);
-			
-			fwrite(buf, 1, sz - 0x30, out);
-			fclose(out);
-			
-			free(buf);
-			
-			break;
-		}
-		
-		case 0x5C000: // Course data
-		{
-			printf("Decrypting course %s to %s...\n", argv[1], argv[2]);
-
-			char *buf = malloc(0x5C000);
-			
-			fread(buf, 1, 0x5C000, in);
-			fclose(in);
-			
-			char *end = buf + sz - 0x30;
-			
-			rand_init(rand_state, *(uint32_t *)&end[0x10], *(uint32_t *)&end[0x14], *(uint32_t *)&end[0x18], *(uint32_t *)&end[0x1C]);
-			gen_key(course_key_table, key_state, rand_state);
-			
-			AES_init_ctx_iv(&ctx, (uint8_t *)key_state, end);		
-			AES_CBC_decrypt_buffer(&ctx, buf + 0x10, sz - 0x40);
-			
-			fwrite(buf + 0x10, 1, sz - 0x40, out);
-			fclose(out);
-
-			free(buf);
-			
-			break;
-		}
-		
-		case 0x68000: // Replay data
-		{
-			printf("Decrypting replay %s to %s...\n", argv[1], argv[2]);
-
-			char *buf = malloc(0x68000);
-			
-			fread(buf, 1, 0x68000, in);
-			fclose(in);
-			
-			char *end = buf + sz - 0x30;
-			
-			rand_init(rand_state, *(uint32_t *)&end[0x10], *(uint32_t *)&end[0x14], *(uint32_t *)&end[0x18], *(uint32_t *)&end[0x1C]);
-			gen_key(replay_key_table, key_state, rand_state);
-			
-			AES_init_ctx_iv(&ctx, (uint8_t *)key_state, end);		
-			AES_CBC_decrypt_buffer(&ctx, buf, sz - 0x30);
-			
-			fwrite(buf, 1, sz - 0x30, out);
-			fclose(out);
-
-			free(buf);
-			
-			break;
-		}
-		
-		default:
-		{
-			puts("Error: Unsupported file!");
-			fclose(in);
-			fclose(out);
-			return 0;
-		}
+		printf("unsupported file: %s\n", inFileName);
+		return -1;
 	}
+
+	char *buf = malloc(file_type->fileSize);
+	memset(buf, 0, file_type->fileSize);
+
+	fread(buf, 1, file_type->fileSize, in);
+	fclose(in);
+
+	char *end = buf + sz - 0x30;
+
+	rand_init(rand_state, *(uint32_t *)&end[0x10], *(uint32_t *)&end[0x14], *(uint32_t *)&end[0x18], *(uint32_t *)&end[0x1C]);
+	gen_key(file_type->key_table, key_state, rand_state);
+
+	AES_init_ctx_iv(&ctx, (uint8_t *)key_state, end);
+
+	if (encrypt)
+	{
+		printf("Encrypting %s %s to %s...\n", file_type->name, inFileName, outFileName);
+		AES_CBC_encrypt_buffer(&ctx, buf + file_type->offset, sz - (0x30 + file_type->offset));
+	}
+	else
+	{
+		printf("Decrypting %s %s to %s...\n", file_type->name, inFileName, outFileName);
+		AES_CBC_decrypt_buffer(&ctx, buf + file_type->offset, sz - (0x30 + file_type->offset));
+	}
+
+	fwrite(buf + file_type->offset, 1, sz - (0x30 + file_type->offset), out);
+	fclose(out);
+
+	free(buf);
 	
 	puts("Done!");
 	
